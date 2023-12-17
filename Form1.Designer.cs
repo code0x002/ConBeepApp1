d88d88b38698fffe97a59097283654c7af194023
code0x002 <47268540+code0x002@users.noreply.github.com>
2023-12-17 18:56:09 +0700

    Init code

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿
+namespace ConBeepApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.trackBarSecLabel = new System.Windows.Forms.Label();
+            this.toolStripStatusLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.Location = new System.Drawing.Point(3, 3);
+            this.trackBar1.Maximum = 15;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(142, 49);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 11;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.flowLayoutPanel2);
+            this.flowLayoutPanel1.Controls.Add(this.toolStripStatusLabel);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(226, 157);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.trackBar1);
+            this.flowLayoutPanel2.Controls.Add(this.trackBarSecLabel);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(223, 59);
+            this.flowLayoutPanel2.TabIndex = 2;
+            // 
+            // trackBarSecLabel
+            // 
+            this.trackBarSecLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trackBarSecLabel.Location = new System.Drawing.Point(151, 0);
+            this.trackBarSecLabel.Name = "trackBarSecLabel";
+            this.trackBarSecLabel.Size = new System.Drawing.Size(65, 52);
+            this.trackBarSecLabel.TabIndex = 2;
+            this.trackBarSecLabel.Text = "11 Sec";
+            this.trackBarSecLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel.Location = new System.Drawing.Point(3, 65);
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(223, 89);
+            this.toolStripStatusLabel.TabIndex = 3;
+            this.toolStripStatusLabel.Text = "11";
+            this.toolStripStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 157);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Con Beep";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label trackBarSecLabel;
+        private System.Windows.Forms.Label toolStripStatusLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConBeepApp1
+{
+    public partial class Form1 : Form
+    {
+        private int secondsToWait = 11;
+        private DateTime startTime;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer.Start(); // start timer (you can do it on form load, if you need)
+            startTime = DateTime.Now; // and remember start time
+        }
+
+        public void Beep()
+        {
+            //Console.Beep(5000, 500);
+
+            System.IO.Stream stream = Properties.Resources.beep_07a;
+            System.Media.SoundPlayer player = new System.Media.SoundPlayer(stream);
+            //player.SoundLocation = @"beep-07a.wav";
+            player.Play(); //or player.PlaySync();
+
+            //stop
+            //player.Stop();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            int elapsedSeconds = (int)(DateTime.Now - startTime).TotalSeconds;
+            int remainingSeconds = secondsToWait - elapsedSeconds;
+
+            if (remainingSeconds <= 0)
+            {
+                toolStripStatusLabel.Text = String.Format("{0}", remainingSeconds);
+
+                Beep();
+                startTime = DateTime.Now; // and remember start time
+            }
+            else
+            {
+                toolStripStatusLabel.Text = String.Format("{0}", remainingSeconds);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            secondsToWait = trackBar1.Value;
+            trackBarSecLabel.Text = trackBar1.Value.ToString() + " Sec";
+        }
+    }
+}
